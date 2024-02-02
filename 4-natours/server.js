118f225a589b463b45132e7e8bc478ef8d9a4763
21120262 <huukhangtc@gmail.com>
2024-02-02 16:15:42 +0700

    feat: basic CRUD RESTapi

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
-// console.log(process.env.PASSWORD);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch(() => {
+    console.log("Couldn't connect to MongoDB");
+  });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
