8715cc428459051f6b5ace070fbec30378cccdf3
21120262 <huukhangtc@gmail.com>
2024-02-04 19:27:40 +0700

    feat: pagiantion

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -15,7 +15,7 @@ exports.getAllTours = async (req, res) => {
     // 1B) Advanced filtering
     let queryStr = JSON.stringify(queryObj)
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-    
+
     let query = Tour.find(JSON.parse(queryStr))
 
     // 2) Sorting
@@ -26,7 +26,7 @@ exports.getAllTours = async (req, res) => {
       query = query.sort(sortBy)
     }
     else {
-      query = query.sort('-createdAt')
+      // query = query.sort('-createdAt')
     }
 
     // 3) Field limiting
@@ -36,6 +36,21 @@ exports.getAllTours = async (req, res) => {
     }
     else {
       query = query.select('-__v')
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1
+    const limit = req.query.limit * 1 || 100
+    const skip = (page - 1) * limit
+
+    console.log('page:', page, 'limit:', limit, 'skip:', skip)
+
+    query = query.skip(skip).limit(limit)
+    
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments()
+      if (skip >= numTours) throw new Error('This page does not exist')
     }
 
     // EXECUTE QUERY
