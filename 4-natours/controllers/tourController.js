a3e2cb810327aeaea011b342174a106c81035087
21120262 <huukhangtc@gmail.com>
2024-02-05 22:01:05 +0700

    module APIFeaturs

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require('../models/tourModel')
-
+const APIFeatures = require('../utils/apiFeatures')
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // )
@@ -9,60 +9,17 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.sort = '-ratingsAverage,price'
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
   next()
-
 }
 
 exports.getAllTours = async (req, res) => {
   try {
-    // BUILD QUERY
-    // 1A) Filtering
-    const queryObj = { ...req.query }
-    const excludedFields = ['page', 'sort', 'limit', 'fields']
-    excludedFields.forEach(el => delete queryObj[el])
-
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-
-    let query = Tour.find(JSON.parse(queryStr))
-
-    // 2) Sorting
-    // console.log(req.query)
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      // console.log(sortBy)
-      query = query.sort(sortBy)
-    }
-    else { //bug
-      // query = query.sort('-createdAt') 
-    }
-
-    // 3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ')
-      query = query.select(fields)
-    }
-    else {
-      query = query.select('-__v')
-    }
-
-    // 4) Pagination
-    const page = req.query.page * 1 || 1
-    const limit = req.query.limit * 1 || 100
-    const skip = (page - 1) * limit
-
-    // console.log('page:', page, 'limit:', limit, 'skip:', skip)
-
-    query = query.skip(skip).limit(limit)
-    
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments()
-      if (skip >= numTours) throw new Error('This page does not exist')
-    }
-
     // EXECUTE QUERY
-    const tours = await query
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate()
+    const tours = await features.query
 
     res.status(200).json({
       status: 'success',
@@ -144,6 +101,40 @@ exports.deleteTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null
+    })
+
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    })
+  }
+}
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: {
+            $max: '$price'
+          }
+        }
+      }
+    ])
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
     })
 
   } catch (err) {
--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router()
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/')
-  .get(tourController.getAllTours)
+  .get(tourController.getAllTours) 
   .post(tourController.createTour);
 
 router.route('/:id')
