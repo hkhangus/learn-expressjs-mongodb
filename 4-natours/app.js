fff7401515953ff743f7972f63a88bff149aae4d
21120262 <huukhangtc@gmail.com>
2024-02-13 22:03:43 +0700

    add class global error handler

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -12,7 +14,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`)); 
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -24,11 +26,21 @@ app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  })
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`
+  // })
+
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
+  // next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 })
+
+app.use(globalErrorHandler)
 
 // 4) START SERVER
 
