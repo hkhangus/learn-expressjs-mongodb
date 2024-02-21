80a0f1806a1bcc302d6cfc168787bc2cc71651e7
21120262 <huukhangtc@gmail.com>
2024-02-21 13:36:02 +0700

    add user role persmission

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,32 +1,38 @@
-const AppError = require('../utils/appError')
+const AppError = require('../utils/appError');
 
-const handleCastErrorDB = err => {
-  const message = `Invalid ${err.path}: ${err.value}.`
-  return new AppError(message, 400)
-}
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 
-const handleDuplicateFieldsDB = err => {
-  const value = err.keyValue.name
-  
-  const message = `Duplicate field value: ${value}. Please use another value!`
-  return new AppError(message, 400)
-}
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.keyValue.name;
 
-const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message)
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
 
-  const message = `Invalid input data. ${errors.join('. ')}`
-  return new AppError(message, 400)
-}
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
     err: err,
-    stack: err.stack
-  })
-}
+    stack: err.stack,
+  });
+};
 
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
@@ -34,8 +40,8 @@ const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message
-    })
+      message: err.message,
+    });
   } else {
     // Programming or other unknown error: don't leak error details
     // 1) Log error
@@ -44,10 +50,10 @@ const sendErrorProd = (err, res) => {
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went very wrong!'
-    })
+      message: 'Something went very wrong!',
+    });
   }
-}
+};
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
@@ -55,16 +61,17 @@ module.exports = (err, req, res, next) => {
   // console.log(err)
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res)
+    sendErrorDev(err, res);
   }
   if (process.env.NODE_ENV === 'production') {
-    let error = { ...err }
-    // console.log(error)
+    let error = { ...err };
 
-    if (err.name === 'CastError') error = handleCastErrorDB(error)
-    if (err.code === 11000) error = handleDuplicateFieldsDB(error)
-    if (err.name === 'ValidationError') error = handleValidationErrorDB(error)
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (err.name === 'JsonWebTokenError') error = handleJWTError();
+    if (err.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-    sendErrorProd(error, res)
+    sendErrorProd(error, res);
   }
-}
+};
--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a passord'],
@@ -34,6 +39,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -54,6 +60,19 @@ userSchema.methods.correctPassword = async function (
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+  //False means NOT changed
+  return false;
+}
 
 const User = mongoose.model('User', userSchema);
 
