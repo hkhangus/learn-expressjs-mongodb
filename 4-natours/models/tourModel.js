1ba941278084928964e1b3d883eeb0114b04e058
21120262 <huukhangtc@gmail.com>
2024-02-27 21:38:07 +0700

    populate tour guide

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -151,11 +151,18 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-// tourSchema.post(/^find/, function(docs, next) {
-//     console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-//     console.log(docs);
-//     next();
-// });
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt',
+  });
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+    next();
+});
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
