f1e8cd906fbcc76e1a1d1f30f239fa35e2d64013
21120262 <huukhangtc@gmail.com>
2024-02-28 12:17:44 +0700

    add review schem

---
+++ 4-natours/models/reviewModel.js
@@ -0,0 +1,55 @@
+// review / rating / createdAt / ref to tour / ref to user
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    rating: {
+      type: Number,
+      // required: [true, 'A review must have a rating!'],
+      // default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+reviewSchema.pre(/^find/, function (next) {
+  this.populate([
+    {
+      path: 'tour',
+      select: '-__v -createdAt',
+    },
+    {
+      path: 'user',
+      select: '-__v -passwordChangedAt',
+    },
+  ]);
+  next();
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
